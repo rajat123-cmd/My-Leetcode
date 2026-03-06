01ff8065df2da2012d4105dcd7d26ae0a9c11f32
Rajat yadav <rajat1yadav23@gmail.com>
2026-03-06 22:48:33 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (5.41%) - LeetSync

---
+++ 120-triangle/triangle.c
@@ -0,0 +1,15 @@
+int minimumTotal(int** triangle, int triangleSize, int* triangleColSize) {
+
+    for(int i = triangleSize - 2; i >= 0; i--){
+        for(int j = 0; j <= i; j++){
+
+            if(triangle[i+1][j] < triangle[i+1][j+1])
+                triangle[i][j] += triangle[i+1][j];
+            else
+                triangle[i][j] += triangle[i+1][j+1];
+
+        }
+    }
+
+    return triangle[0][0];
+}

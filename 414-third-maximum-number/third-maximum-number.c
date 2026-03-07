a5d3243b52fc4f6fce86770b740e3f267f48fd20
Rajat yadav <rajat1yadav23@gmail.com>
2026-03-07 20:32:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (7.33%) - LeetSync

---
+++ 414-third-maximum-number/third-maximum-number.c
@@ -0,0 +1,30 @@
+#include <limits.h>
+
+int thirdMax(int* nums, int numsSize) {
+    long first = LONG_MIN, second = LONG_MIN, third = LONG_MIN;
+
+    for(int i = 0; i < numsSize; i++) {
+        int n = nums[i];
+
+        if(n == first || n == second || n == third)
+            continue;
+
+        if(n > first) {
+            third = second;
+            second = first;
+            first = n;
+        }
+        else if(n > second) {
+            third = second;
+            second = n;
+        }
+        else if(n > third) {
+            third = n;
+        }
+    }
+
+    if(third == LONG_MIN)
+        return first;
+    else
+        return third;
+}
